f337c3017a6be40b3e19d897ebdba32d944d62e1
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-02-10 01:16:59 +0900

    add req-res-baisc

---
+++ req-res-basic/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // http://localhost:3000/　にアクセスすると表示される
+  console.log(req);
+  // スラッグを変えたら変わる
+  console.log(req.url);
+  console.log(req.method);
+  // レスポンスを送信すると初めてブラウザの画面表示が決まる
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.write('test');
+  // コンソールのネットワークでリロードするとlocalhost→ヘッダーをクリックすると確認できる
+
+  // HTMLを表示する
+  res.setHeader('Content-Type', 'text/html');
+  // res.write('<h1>Title</h1>');
+  // res.end();
+
+  // fsをインポートして表示する(CSSが使えるようになる)
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      // res.end();
+      // これでもOK
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('ポート3000！');
+});
